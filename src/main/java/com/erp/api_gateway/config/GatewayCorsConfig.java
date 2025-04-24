d277ead86c093a7c58bb3b99976a6de261bf7391
iammvishnupriya <vishdhana35@gmail.com>
2025-04-24 10:02:46 +0530

    API GATEWAY CONGIG ADDED

--- src/main/java/com/erp/api_gateway/config/GatewayCorsConfig.java
+++ src/main/java/com/erp/api_gateway/config/GatewayCorsConfig.java
@@ -3,40 +3,25 @@ package com.erp.api_gateway.config;
 
 
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.cors.CorsConfiguration;
-
 import org.springframework.web.cors.reactive.CorsWebFilter;
-
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
-
 public class GatewayCorsConfig {
 
     @Bean
-
     public CorsWebFilter corsWebFilter() {
-
         CorsConfiguration config = new CorsConfiguration();
-
         config.setAllowCredentials(true);
-
         config.addAllowedOrigin("http://localhost:4000"); // Your React app
-
         config.addAllowedHeader("*");
-
         config.addAllowedMethod("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
         source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
-
     }
-
 }
-
